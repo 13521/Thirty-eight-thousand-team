270a173bccf917d14b91e9dce68db3edb22bae23
Zhang <823253467@qq.com>
2018-01-19 15:20:10 +0800

    master code

---
+++ QuarterHour/app/src/main/java/com/bawei/quarterhour/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bawei.quarterhour.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bawei.quarterhour.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
